3e647b2f302bb5db819e419b74221ec26becc92f
Jiamu Sun <barroit@linux.com>
2024-10-30 15:50:36 +0900

    br: update
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- asset/umount-onedrive.sh
+++ asset/umount-onedrive.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 # SPDX-License-Identifier: GPL-3.0-only
 
+if [[ ! $ONESHOT_LIST ]]; then
+	exit 0
+fi
+
 while read; do
 	umount $REPLY
+	rm -f "$ONESHOT_LIST/onedrive~$(basename $REPLY)"
 done < <(mount | grep rclone | awk '{ print $3 }')
