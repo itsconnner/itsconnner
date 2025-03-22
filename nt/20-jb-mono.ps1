d44925d0b3662d42cadd61e59892ed1fc67c6824
Jiamu Sun <barroit@linux.com>
2025-03-22 23:55:00 +0900

    nt: automate jetbrains mono installation
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ nt/20-jb-mono.ps1
@@ -0,0 +1,63 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+$dst = "$Env:LOCALAPPDATA\Microsoft\Windows\Fonts"
+
+if ((Get-ChildItem -Filter JetBrainsMono-* $dst).Count) {
+	log "Installing JetBrains Mono ... Skipped"
+	exit
+}
+
+$url = Get-Content $PSScriptRoot\..\def\url.jbmono
+$tmp = ".tmp-$PID"
+$zip = "$tmp/$(Split-Path $url -Leaf)"
+
+New-Item -ItemType directory $tmp >nul
+
+curl -L -o $zip $url
+if (-not $?) {
+	Remove-Item -Recurse -Force $tmp
+	die "JetBrains Mono download url ``$url' is outdated"
+}
+
+Expand-Archive $zip $tmp
+
+$pattern = "$tmp\fonts\ttf\*"
+$src = Get-ChildItem -Name $pattern
+
+if (-not (Test-Path $dst)) {
+	New-Item -ItemType directory $dst >nul
+}
+
+Copy-Item $pattern $dst
+
+$reg = "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+$styles = 'Thin',
+	  'ExtraLight',
+	  'Light',
+	  'Regular',
+	  'Medium',
+	  'SemiBold',
+	  'Bold',
+	  'ExtraBold',
+	  'Black',
+	  'Italic'
+$names = 'JetBrains',
+	 'Mono',
+	 'NL'
+
+foreach ($font in $src) {
+	$seg = $font -split '-'
+	$family = $seg[0]
+	$style = $seg[1]
+
+	$style = ($styles | Where-Object { $style -match $_ }) -join ' '
+	$family = ($names | Where-Object { $family -match $_ }) -join ' '
+	$key = "$family $style (TrueType)"
+
+	New-ItemProperty -ErrorAction SilentlyContinue `
+			 -PropertyType String -Value $dst\$font $reg $key >nul
+}
+
+log "Installing JetBrains Mono ... OK"
+
+Remove-Item -Recurse -Force $tmp
