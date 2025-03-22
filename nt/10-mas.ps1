cf010a584bc4622b9a1f5c2a7fc71d7d1cb8c3e7
Jiamu Sun <barroit@linux.com>
2025-03-22 23:53:09 +0900

    nt: automate windows activation
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ nt/10-mas.ps1
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+function is_activated
+{
+	$product = Get-CimInstance -Query "SELECT LicenseStatus `
+					   FROM SoftwareLicensingProduct `
+					   WHERE PartialProductKey IS NOT NULL"
+	$state = $product.LicenseStatus
+
+	return $state -eq 1
+}
+
+if (is_activated) {
+	log "Activating Windows ... Skipped"
+	exit
+}
+
+$domain = Get-Content $PSScriptRoot\mas_domain
+
+Resolve-DnsName -ErrorAction SilentlyContinue $domain >nul
+if (-not $?) {
+	die "Microsoft Activation Scripts domain ``$domain' is outdated"
+}
+
+Invoke-RestMethod https://$domain | Invoke-Expression
+if (-not (is_activated)) {
+	die 'Windows activation failed'
+}
+
+log "Activating Windows ... OK"
