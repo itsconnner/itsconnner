a8fc00ac772166276ac6ef16f148af1eb87c8a60
Jiamu Sun <barroit@linux.com>
2025-03-23 00:50:40 +0900

    nt: support running specific setup script by name
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- nt/setup.ps1
+++ nt/setup.ps1
@@ -2,12 +2,40 @@
 
 #requires -version 7
 
+. $PSScriptRoot\libkit.ps1
+
 $scripts = Get-ChildItem -Name -Filter ??-*.ps1 $PSScriptRoot
+
+if ($args.Length) {
+	$name = $args[0]
+
+	if ($name -notmatch '^\d\d-.+\.ps1$') {
+		$script = $scripts | Where-Object {
+			$_ -match "^\d\d-$name.*\.ps1$"
+		}
+
+		if ($script -is [array]) {
+			$ambig = $script | ForEach-Object { "`n  $_" }
+
+			die "ambiguous script ``$name'; could be:$ambig"
+		} elseif ($script -isnot [string]) {
+			die "unknown script ``$name'"
+		}
+	} elseif (-not (Test-Path $PSScriptRoot\$name)) {
+		die "unknown script ``$name'"
+	} else {
+		$script = $name
+	}
+
+	log "Executing $PSScriptRoot\$script"
+	& $PSScriptRoot\$script
+	exit
+}
 
 foreach ($script in $scripts) {
 	pwsh -Command ". $PSScriptRoot\libkit.ps1; & $PSScriptRoot\$script"
 
 	if (-not $?) {
-		warn "$PSScriptRoot\$script setup is not complete"
+		warn "$PSScriptRoot\$script interrupted"
 	}
 }
