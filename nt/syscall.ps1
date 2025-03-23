6030f6f6faa5e9af5c2295f7b6cd22872fc4bb16
Jiamu Sun <barroit@linux.com>
2025-03-23 21:39:17 +0900

    nt: add send_notify_message() syscall mapping
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- nt/syscall.ps1
+++ nt/syscall.ps1
@@ -1,37 +1,47 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-Add-Type -TypeDefinition @'
+Add-Type @'
 using System;
 using System.Runtime.InteropServices;
 
 public class syscall
 {
-	[DllImport("user32.dll")]
-	private static extern bool SetSysColors(int n, int[] el, int[] colors);
 
-	[DllImport("user32.dll")]
-	private static extern int SystemParametersInfo(int act, int p1,
-						       string p2, int flags);
+[DllImport("user32.dll")]
+private static extern bool SetSysColors(int n, int[] el, int[] colors);
 
-	[DllImport("shell32.dll")]
-	private static extern void SHChangeNotify(int id, uint flags,
-						  IntPtr i1, IntPtr i2);
+[DllImport("user32.dll")]
+private static extern int SystemParametersInfo(int act, int p1,
+					       string p2, int flags);
 
-	public static void sys_colors(int n, int[] el, int[] colors)
-	{
-		SetSysColors(n, el, colors);
-	}
+[DllImport("shell32.dll")]
+private static extern void SHChangeNotify(int id, uint flags,
+					  IntPtr i1, IntPtr i2);
 
-	public static void sys_param_info(int act,
-					  int p1, string p2, int flags)
-	{
-		SystemParametersInfo(act, p1, p2, flags);
-	}
+[DllImport("user32.dll")]
+public static extern bool SendNotifyMessage(IntPtr wnd, uint msg,
+					    UIntPtr wp, IntPtr lp);
 
-	public static void sh_change_notify(int id, uint flags,
-					    IntPtr i1, IntPtr i2)
-	{
-		SHChangeNotify(id, flags, i1, i2);
-	}
+public static void sys_colors(int n, int[] el, int[] colors)
+{
+	SetSysColors(n, el, colors);
 }
+
+public static void sys_param_info(int act, int p1, string p2, int flags)
+{
+	SystemParametersInfo(act, p1, p2, flags);
+}
+
+public static void sh_change_notify(int id, uint flags, IntPtr i1, IntPtr i2)
+{
+	SHChangeNotify(id, flags, i1, i2);
+}
+
+public static void send_notify_message(IntPtr wnd, uint msg,
+				       UIntPtr wp, IntPtr lp)
+{
+	SendNotifyMessage(wnd, msg, wp, lp);
+}
+
+} /* class syscall */
 '@
