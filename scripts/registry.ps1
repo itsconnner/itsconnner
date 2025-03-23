11b275a442e551907025063ae8177bbb7398cc4e
Jiamu Sun <barroit@linux.com>
2025-03-23 21:40:27 +0900

    scripts/registry: automate registry comparison
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ scripts/registry.ps1
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#requires -version 7
+
+param([ArgumentCompletions('dump', 'cmp')]
+      [string]$action)
+
+. $PSScriptRoot\..\nt\libkit.ps1
+
+if (-not $action) {
+	die "missing action, available actions are:`n  dump`n  cmp"
+}
+
+switch ($action) {
+'dump' {
+	reg query $args[0] 2>&1 >nul
+
+	if (-not $?) {
+		die "invalid registry path ``$($args[0])'"
+	}
+
+	if (Test-Path .redump) {
+		Move-Item -Force .redump .redump.old
+	}
+
+	reg export $args[0] .redump
+}
+'cmp' {
+	if (-not (Test-Path .redump.old)) {
+		die 'not enough redumps to compare'
+	}
+
+	$new = Get-Content .redump
+	$old = Get-Content .redump.old
+
+	Compare-Object $new $old
+}
+}
