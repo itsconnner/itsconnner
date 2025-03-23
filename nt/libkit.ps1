itsconnner/itsconnner

commit 41cfc6d134310ff52f2416948df151f2370d595b

nt: load registry key from version-spec file
Signed-off-by: Jiamu Sun <barroit@linux.com>

nt/libkit.ps1
@@ -7,6 +7,8 @@ $CYAN	= "`e[0;36m"
 $WHITE	= "`e[0;37m"
 $RESET	= "`e[0m"
 
+$VERSION = [System.Environment]::OSVersion.Version.Major
+
 function die
 {
 	Write-Output "${RED}fatal:${RESET} $args"
@@ -23,6 +25,11 @@ function warn
 	Write-Output "${YELLOW}warn:${RESET} $args"
 }
 
+function note
+{
+	Write-Output "${CYAN}note:${RESET} $args"
+}
+
 function uptime
 {
 	$prev = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
@@ -35,4 +42,37 @@ function uptime
 function log
 {
 	Write-Output "${GREEN}[$(uptime)]${RESET} $args"
+}
+
+function ver-spec-name
+{
+	$name = "$PSScriptRoot\..\windows-$($VERSION)\$($args[0])"
+
+	if (-not (Test-Path $name)) {
+		die "version-specified file ``$name' doesn't exist"
+	}
+
+	return $name
+}
+
+function load-pair
+{
+	$ret = @{}
+	$sep = '\t'
+
+	if ($args.Length -gt 1) {
+		$sep = $args[1]
+	}
+
+	Get-Content $args[0] | ForEach-Object {
+		$pair = $_ -split $sep
+
+		if ($pair.Length -eq 2) {
+			$ret[$pair[0]] = $pair[1]
+		} else {
+			$ret[$pair[0]] = $pair[1..($pair.Length - 1)]
+		}
+	}
+
+	return $ret
 }

nt/20-jb-mono.ps1
@@ -30,7 +30,10 @@ if (-not (Test-Path $dst)) {
 
 Copy-Item $pattern $dst
 
-$reg = "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+$def_path = ver-spec-name jb-mono
+$def = load-pair $def_path
+$reg = $def['reg']
+
 $styles = 'Thin',
 	  'ExtraLight',
 	  'Light',

windows-10/jb-mono
@@ -1,0 +1,1 @@
+reg	HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Fonts

commit cff9dc7366c323bdb8c830447305ee2c5b1392bd

nt: fix reading empty lines in load-pair()
Fixes: 41cfc6d13431 ("nt: load registry key from version-spec file")
Signed-off-by: Jiamu Sun <barroit@linux.com>

nt/libkit.ps1
@@ -67,7 +67,11 @@ function load-pair
 	}
 
 	Get-Content $args[0] | ForEach-Object {
-		$pair = $_ -split $sep
+		if (-not $_ -or $_ -match '^#') {
+			return
+		}
+
+		$pair = $_ -split $sep | Where-Object { $_ }
 
 		if ($pair.Length -eq 2) {
 			$ret[$pair[0]] = $pair[1]