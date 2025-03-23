5ab3b210859ceb47d44d2a8cb791e7ae54bd026e
Jiamu Sun <barroit@linux.com>
2025-03-23 22:02:52 +0900

    scripts/vmksign: automate vmware kernel module signing
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ scripts/vmksign.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+source $(dirname $(echo $0))/../linux/libkit.sh
+
+if [[ ! -f ~/.mok/vmware || ! -f ~/.mok/vmware.der ]]; then
+	error 'VMware Workstation MOKs are not found'
+
+	if ! confirm "Enroll the keys?"; then
+		warn "VMware Workstation kernel module signing was interrupted"
+		exit 0
+	fi
+
+	if ! openssl version | grep -q '3\.'; then
+		die 'missing 3.x openssl'
+	fi
+
+	openssl genpkey -out ~/.mok/vmware \
+			-algorithm RSA -pkeyopt rsa_keygen_bits:4096
+
+	openssl req -new -x509 -key ~/.mok/vmware \
+		    -outform DER -out ~/.mok/vmware.der -days 36500
+
+	sudo mokutil --import ~/.mok/vmware.der
+
+	if ! confirm "Reboot now?"; then
+		note "VMware Workstation kernel module signing is incomplete"
+		exit 0
+	fi
+
+	sudo reboot
+fi
+
+for module in vmmon vmnet; do
+	sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file \
+	     sha256 ~/.mok/vmware ~/.mok/vmware.der $(modinfo -n $module)
+
+	sudo modprobe -r $module
+	sudo modprobe $module
+done
+
+log "Signing VMware Workstation kernel modules ... OK"
