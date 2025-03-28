22a7389193e6deb77509425d981a5472c7c10c98
Jiamu Sun <barroit@linux.com>
2025-03-29 00:48:09 +0900

    wrefactor: restructure configuration file hierarchy
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ assets/gdb/hexdump.py
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-3.0-only
+
+class HexDump(gdb.Command):
+	def __init__(self):
+		super(HexDump, self).__init__("hexdump", gdb.COMMAND_DATA)
+
+	def invoke(self, arg, from_tty):
+		args = gdb.string_to_argv(arg)
+		if len(args) != 2:
+			raise gdb.GdbError('hexdump requires 2 arguments: address and length')
+
+		address = int(gdb.parse_and_eval(args[0]))
+		length = int(gdb.parse_and_eval(args[1]))
+
+		for i in range(0, length, 16):
+			line_address = address + i
+
+			memory = gdb.selected_inferior().read_memory(line_address, 16)
+			bytes_list = bytearray(memory)
+
+			hex_values = ' '.join('{:02x}'.format(b) for b in bytes_list)
+			ascii_values = ''.join(chr(b) if 32 <= b < 127 else '.' for b in bytes_list)
+
+			print(f"{line_address:#010x}: {hex_values:<48} |{ascii_values}|")
+
+HexDump()
---
+++ assets/vim/.vimrc
@@ -0,0 +1,20 @@
+call plug#begin('~/.vim/plugged')
+" List your plugins here
+Plug 'octol/vim-cpp-enhanced-highlight'
+call plug#end()
+
+if !isdirectory(expand('~/.vim/swap'))
+	silent !mkdir -p ~/.vim/swap
+endif
+
+set directory=~/.vim/swap//	" swap folder
+
+set number			" line numbers
+set tabstop=8
+set autoindent
+set shiftwidth=8
+set cindent			" C/C++ indentation
+set backspace=indent,eol,start	" backspace behavior
+set ruler			" column number
+
+colorscheme barroit
