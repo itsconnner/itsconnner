itsconnner/itsconnner

commit 1e385ec95ac49dc9fb9f4c71246c42fb7e59040e

windows: automate configuration file linking
Signed-off-by: Jiamu Sun <barroit@linux.com>

config/configs-windows
@@ -1,0 +1,12 @@
+code/keybindings.json	$Env:APPDATA\Code\User
+code/settings.json	$Env:APPDATA\Code\User
+
+git/.gitconfig
+git/ignore
+
+gpg/pgp.conf		$Env:APPDATA\gnupg
+
+ssh/config
+
+vim/.vimrc
+vim/barroit.vim

windows/50-configs.ps1
@@ -1,0 +1,28 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# NO MORE SYMLINKS ON WINDOWS. THE DEFAULT FILE SYSTEM FUCKED ME UP :(
+
+$config = read-pair $PSScriptRoot\..\config\configs
+
+foreach ($line in (read-line $PSScriptRoot\..\config\configs-windows)) {
+	$col = $line -split '\t'
+	$path = $col[0]
+
+	$dir = ($config[$path] -split '\t')[0]
+	$name = ($path -split '/')[1]
+
+	if ($col.Count -gt 1) {
+		$col = $col | Where-Object { $_ }
+		$dir = Invoke-Expression "`"$($col[1])`""
+	}
+
+	if (-not (Test-Path $dir)) {
+		New-Item -ItemType Directory $dir >NUL
+	}
+
+	$dst = Resolve-Path $dir/$name
+
+	Copy-Item -Force "$PSScriptRoot/../assets/$path" $dst >NUL
+	log ("$CYAN{0,-25}$RESET -> $GREEN$dst$RESET" -f $path)
+}
+
+log "Linking configuration files ... OK"

config/configs
@@ -1,0 +1,27 @@
+code/keybindings.json	~/.config/Code/User
+code/settings.json	~/.config/Code/User
+
+fastfetch/config.jsonc	~/.config/fastfetch
+fastfetch/dotmiku	~/.config/fastfetch	copy
+
+gdb/gdbinit		~/.config/gdb
+gdb/hexdump.py		~/.config/gdb
+
+git/.gitconfig		~
+git/ignore		~/.config/git
+
+gpg/pgp.conf		~/.gnupg
+
+mutt/.mailcap		~
+mutt/mutt_oauth2.py	~/.mutt
+mutt/.muttrc		~
+mutt/.signature		~
+
+ssh/config		~/.ssh
+
+vim/.vimrc		~
+vim/barroit.vim		~/.vim/colors
+
+xmodmap/.Xmodmap	~
+
+xorg/xorg.conf		/etc/X11		copy,sudo

commit b717653a9815f91ab0336c9bbaf5ae6a68eac37c

windows: fix incorrect vim runtime path
Fixes: 1e385ec95ac4 ("windows: automate configuration file linking")
Signed-off-by: Jiamu Sun <barroit@linux.com>

config/configs-windows
@@ -9,4 +9,4 @@ gpg/pgp.conf		$Env:APPDATA\gnupg
 ssh/config
 
 vim/.vimrc
-vim/barroit.vim
+vim/barroit.vim		$Env:USERPROFILE\vimfiles\colors

windows/50-configs.ps1
@@ -19,7 +19,7 @@ foreach ($line in (read-line $PSScriptRoot\..\config\configs-windows)) {
 		New-Item -ItemType Directory $dir >NUL
 	}
 
-	$dst = Resolve-Path $dir/$name
+	$dst = "$(Resolve-Path $dir)\$name"
 
 	Copy-Item -Force "$PSScriptRoot/../assets/$path" $dst >NUL
 	log ("$CYAN{0,-25}$RESET -> $GREEN$dst$RESET" -f $path)