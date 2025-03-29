9485d563134f8888147a844ec7a217f82967417d
Jiamu Sun <barroit@linux.com>
2025-03-29 13:19:55 +0900

    windows: configure sshd service after installation
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- windows/20-openssh.ps1
+++ windows/20-openssh.ps1
@@ -2,10 +2,15 @@
 
 if (is-admin) {
 	Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+	Start-Service sshd
+	Set-Service sshd -StartupType Automatic
+
+	note 'Start sshd once as administrator to make it function properly'
 	exit
 }
 
-if (Get-Command -ErrorAction SilentlyContinue sshd) {
+if (-not (sr_is_force $args) -and (Get-Command sshd 2>NUL)) {
 	log 'Installing openssh ... Skipped'
 	exit
 }
