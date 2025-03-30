itsconnner/itsconnner

commit 5bee11e3200e562d43048f9cd4c5d68bdf784854

windows: fix incorrect key name
Fixes: 27f2a71ae6de ("windows: automate key copying")
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/20-keys.ps1
@@ -11,7 +11,7 @@ if (-not (Test-Path L:\)) {
 	die "LOCKER did not mount to L:"
 }
 
-$tmpd = "L:\$PID"
+$tmpd = "L:\.tmp-$PID"
 $rule = New-Object $fsrule($env:USERNAME, 'Read', 'Allow')
 
 New-Item -ItemType Directory $tmpd >NUL
@@ -21,14 +21,20 @@ foreach ($sec in (Get-ChildItem -Filter *.gpg -Name L:\)) {
 	switch -Regex ($sec) {
 	'^pg' {
 		gpg --import L:\$sec
+		if (-not $?) {
+			warn "Importing $sec ... Failed"
+			continue
+		}
 	}
 	'^id' {
 		gpg -o $tmpd\$sec -d L:\$sec
 		if (-not $?) {
+			warn "Importing $sec ... Failed"
 			continue
 		}
 
-		$dst = "$HOME\.ssh\$sec"
+		$name = (Get-Item $sec).BaseName
+		$dst = "$HOME\.ssh\$name"
 
 		Remove-Item -ErrorAction SilentlyContinue $dst
 		Move-Item $tmpd\$sec $dst
@@ -45,9 +51,11 @@ foreach ($sec in (Get-ChildItem -Filter *.gpg -Name L:\)) {
 		Set-Acl $dst $acl
 	}
 	}
+
+	log "Importing $sec ... DONE"
 }
 
-Remove-Item $tmpd
+Remove-Item -Force $tmpd
 
 sr_done (script_name)
 log 'Copying private keys ... OK'

commit f53ae7779e201eee6d4435d65e5a86bb74cc76c9

windows/keys: fix incorrect path to key file
Fixes: 5bee11e3200e ("windows: fix incorrect key name")
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/20-keys.ps1
@@ -33,7 +33,7 @@ foreach ($sec in (Get-ChildItem -Filter *.gpg -Name L:\)) {
 			continue
 		}
 
-		$name = (Get-Item $sec).BaseName
+		$name = (Get-Item $tmpd\$sec).BaseName
 		$dst = "$HOME\.ssh\$name"
 
 		Remove-Item -ErrorAction SilentlyContinue $dst