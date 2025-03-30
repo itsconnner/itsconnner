itsconnner/itsconnner

commit 27f2a71ae6dee7a517c2edc897631516d29a946d

windows: automate key copying
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/20-keys.ps1
@@ -1,0 +1,53 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+$fsrule = [System.Security.AccessControl.FileSystemAccessRule]
+
+if (-not (sr_is_force $args) -and (sr_is_done (script_name))) {
+	log 'Copying private keys ... Skipped'
+	exit
+}
+
+if (-not (Test-Path L:\)) {
+	die "LOCKER did not mount to L:"
+}
+
+$tmpd = "L:\$PID"
+$rule = New-Object $fsrule($env:USERNAME, 'Read', 'Allow')
+
+New-Item -ItemType Directory $tmpd >NUL
+
+foreach ($sec in (Get-ChildItem -Filter *.gpg -Name L:\)) {
+	log "Importing $sec ..."
+	switch -Regex ($sec) {
+	'^pg' {
+		gpg --import L:\$sec
+	}
+	'^id' {
+		gpg -o $tmpd\$sec -d L:\$sec
+		if (-not $?) {
+			continue
+		}
+
+		$dst = "$HOME\.ssh\$sec"
+
+		Remove-Item -ErrorAction SilentlyContinue $dst
+		Move-Item $tmpd\$sec $dst
+
+		$acl = Get-Acl $dst
+
+		$acl.SetAccessRuleProtection($true, $false)
+
+		$acl.Access | ForEach-Object {
+			$acl.RemoveAccessRule($_)
+		}
+
+		$acl.AddAccessRule($rule)
+		Set-Acl $dst $acl
+	}
+	}
+}
+
+Remove-Item $tmpd
+
+sr_done (script_name)
+log 'Copying private keys ... OK'

commit 5bee11e3200e562d43048f9cd4c5d68bdf784854

windows: fix incorrect key name
Fixes: 27f2a71ae6de ("windows: automate key copying")
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/20-keys.ps1
@@ -11,7 +11,7 @@ if (-not (Test-Path L:\)) {
 	die "LOCKER did not mount to L:"
 }
 
-$tmpd = "L:\$PID"
+$tmpd = "L:\.tmp-$PID"
 $rule = New-Object $fsrule($env:USERNAME, 'Read', 'Allow')
 
 New-Item -ItemType Directory $tmpd >NUL
@@ -21,14 +21,20 @@ foreach ($sec in (Get-ChildItem -Filter *.gpg -Name L:\)) {
 	switch -Regex ($sec) {
 	'^pg' {
 		gpg --import L:\$sec
+		if (-not $?) {
+			warn "Importing $sec ... Failed"
+			continue
+		}
 	}
 	'^id' {
 		gpg -o $tmpd\$sec -d L:\$sec
 		if (-not $?) {
+			warn "Importing $sec ... Failed"
 			continue
 		}
 
-		$dst = "$HOME\.ssh\$sec"
+		$name = (Get-Item $sec).BaseName
+		$dst = "$HOME\.ssh\$name"
 
 		Remove-Item -ErrorAction SilentlyContinue $dst
 		Move-Item $tmpd\$sec $dst
@@ -45,9 +51,11 @@ foreach ($sec in (Get-ChildItem -Filter *.gpg -Name L:\)) {
 		Set-Acl $dst $acl
 	}
 	}
+
+	log "Importing $sec ... DONE"
 }
 
-Remove-Item $tmpd
+Remove-Item -Force $tmpd
 
 sr_done (script_name)
 log 'Copying private keys ... OK'