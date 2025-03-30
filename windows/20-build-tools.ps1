itsconnner/itsconnner

commit 1caff74f53c422eba4e957a38a12f9d5046dbbda

windows/build-tools: make build tools installation dedicated
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/20-build-tools.ps1
@@ -1,0 +1,23 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if (-not (sr_is_force $args) -and (sr_is_done (script_name))) {
+	log 'Installing BuildTools ... Skipped'
+	exit
+}
+
+$id = 'Microsoft.VisualStudio.2022.BuildTools'
+
+if ($Env:PROCESSOR_ARCHITECTURE -eq 'ARM64') {
+	$vctool = 'Microsoft.VisualStudio.Component.VC.Tools.ARM64'
+} else {
+	$vctool = 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64'
+}
+
+$sdk = 'Microsoft.VisualStudio.Component.Windows11SDK.26100'
+
+winget install --id=$id --custom="--add=$vctool --add=$sdk"
+
+env-path-append "${Env:PROGRAMFILES(x86)}\Microsoft Visual Studio\Installer"
+
+sr_done (script_name)
+log 'Installing BuildTools ... OK'

config/winget-packages
@@ -5,7 +5,6 @@ gp	Git.Git
 
 dev	Kitware.CMake
 dev	LLVM.LLVM
-dev	Microsoft.VisualStudio.2022.BuildTools	${Env:PROGRAMFILES(x86)}\Microsoft Visual Studio\Installer	--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows11SDK.26100
 dev	StrawberryPerl.StrawberryPerl
 
 gp	GnuPG.GnuPG

commit 535dfcd44d2c44c5d1c0eaf9218af403d62718f6

windows/build-tools: fix incorrect parameter format
Fixes: 1caff74f53c4 ("windows/build-tools: make build tools installation dedicated")
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/20-build-tools.ps1
@@ -15,7 +15,7 @@ if ($Env:PROCESSOR_ARCHITECTURE -eq 'ARM64') {
 
 $sdk = 'Microsoft.VisualStudio.Component.Windows11SDK.26100'
 
-winget install --id=$id --custom="--add=$vctool --add=$sdk"
+winget install --id=$id --custom="`"--add $vctool --add $sdk`""
 
 env-path-append "${Env:PROGRAMFILES(x86)}\Microsoft Visual Studio\Installer"
 