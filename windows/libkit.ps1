itsconnner/itsconnner

commit e8c5e41e81ffaaba93e04b6920c3b83299b75273

windows/libkit: suppress Get-ItemPropertyValue error messages
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/libkit.ps1
@@ -97,11 +97,7 @@ function sr_done
 
 function sr_is_done
 {
-	try {
-		$ret = Get-ItemPropertyValue $SR_PATH $args[0]
-	} catch {
-		return $false
-	}
+	$ret = Get-ItemPropertyValue $SR_PATH $args[0] 2>NUL
 
 	return $ret -eq 1
 }

commit 2219a093a27a0d4316ed05201ca99b781e1d05ee

windows/libkit: suppress Get-ItemPropertyValue error properly
Fixes: e8c5e41e81ff ("windows/libkit: suppress Get-ItemPropertyValue error messages")
Signed-off-by: Jiamu Sun <barroit@linux.com>

windows/libkit.ps1
@@ -97,7 +97,8 @@ function sr_done
 
 function sr_is_done
 {
-	$ret = Get-ItemPropertyValue $SR_PATH $args[0] 2>NUL
+	$ret = Get-ItemPropertyValue -ErrorAction SilentlyContinue `
+				     $SR_PATH $args[0] 2>NUL
 
 	return $ret -eq 1
 }