itsconnner/itsconnner

commit eec5634e736297d78313bd912f860c8942fd510a

ubuntu: automate power mode adjustment
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/libsetup.sh
@@ -19,3 +19,23 @@ exec_is_foce()
 {
 	test $SCRIPT_PATH = "$FORCE_EXEC"
 }
+
+v1()
+{
+	grep $2 $1 | cut -f2
+}
+
+v2()
+{
+	grep $2 $1 | cut -f3
+}
+
+v3()
+{
+	grep $2 $1 | cut -f4
+}
+
+v4()
+{
+	grep $2 $1 | cut -f5
+}

linux/libkit.sh
@@ -11,3 +11,13 @@ function current()
 {
 	cut -d' ' -f2 /proc/uptime
 }
+
+function virt()
+{
+	test $(systemd-detect-virt) != none
+}
+
+function laptop()
+{
+	ls /sys/class/power_supply/BAT* >/dev/null 2>&1
+}

ubuntu/10-power.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log 'Adjusting power mode ... Skipped'
+	exit
+fi
+
+if ! virt; then
+	powerprofilesctl set power-saver
+else
+	powerprofilesctl set performance
+fi
+
+type=desk
+power=ac
+
+if laptop; then
+	type=lap
+	power=battery
+fi
+
+blank=$(v1 $CONFIG_ROOT/power-mode ${type}_blank)
+suspend=$(v1 $CONFIG_ROOT/power-mode ${type}_suspend)
+
+gsettings set org.gnome.desktop.session idle-delay $blank
+
+gsettings set org.gnome.settings-daemon.plugins.power \
+	  sleep-inactive-$power-timeout $(( $suspend * 60))
+
+gsettings set org.gnome.settings-daemon.plugins.power \
+	  sleep-inactive-$power-type 'suspend'
+
+setup_done
+log 'Adjusting power mode ... OK'

commit 2300da8aa9e8ea54cd70a2c7b70a65c5c137700c

libsetup: fix r*() not handling fields split by multiple tabs
Fixes: eec5634e7362 ("ubuntu: automate power mode adjustment")
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/libsetup.sh
@@ -22,20 +22,20 @@ exec_is_foce()
 
 v1()
 {
-	grep $2 $1 | cut -f2
+	grep $2 $1 | awk '{print $2}'
 }
 
 v2()
 {
-	grep $2 $1 | cut -f3
+	grep $2 $1 | awk '{print $3}'
 }
 
 v3()
 {
-	grep $2 $1 | cut -f4
+	grep $2 $1 | awk '{print $4}'
 }
 
 v4()
 {
-	grep $2 $1 | cut -f5
+	grep $2 $1 | awk '{print $5}'
 }