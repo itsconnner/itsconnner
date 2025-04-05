54427e6dc86a9ab8cd9bad4ea931365a0bd04847
Jiamu Sun <barroit@linux.com>
2025-04-06 08:58:52 +0900

    ubuntu/font: automate font installation
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ ubuntu/70-font.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log "Installing JetBrains Mono ... Skipped"
+	exit
+fi
+
+url=$(cat $CONFIG_ROOT/jb-mono-url)
+zip=$(basename $url)
+
+tmp=$(mktemp -d)
+dst=~/.local/share/fonts/jetbrains-mono
+
+trap 'rm -rf "$tmp"' EXIT
+
+cd $tmp
+
+curl -L -o $zip $url
+unzip $zip
+
+rm -rf $dst
+mv fonts $dst
+
+fc-cache -f
+
+setup_done
+log "Installing JetBrains Mono ... OK"
