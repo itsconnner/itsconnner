484a89669b2011dd654de525f63a7425881565ff
Jiamu Sun <barroit@linux.com>
2025-04-07 03:02:20 +0900

    cleanup: remove legacy scripts
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/setup-gnome-terminal.sh
+++
@@ -1,3 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-dconf load /org/gnome/terminal/legacy/profiles:/ < "$CONFLIST/gnome-terminal"
--- scripts/setup-perfmon.sh
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-COMMENT='#'
-
-SUPERUSER=1
-IOTARGET='/etc/sysctl.d/br-perfmon.conf'
-  write_on_missing 'kernel.perf_event_paranoid = -1'
-  sudo service procps reload
