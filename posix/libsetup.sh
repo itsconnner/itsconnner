itsconnner/itsconnner

commit 0267f6ea77d152a4606a5345b70ca8016e0fbfa2

ubuntu/file-map: automate configuration file linking
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/libsetup.sh
@@ -39,3 +39,24 @@ v4()
 {
 	grep $2 $1 | awk '{print $5}'
 }
+
+
+r1()
+{
+	printf %s "$1" | awk '{print $1}'
+}
+
+r2()
+{
+	printf %s "$1" | awk '{print $2}'
+}
+
+r3()
+{
+	printf %s "$1" | awk '{print $3}'
+}
+
+r4()
+{
+	printf %s "$1" | awk '{print $4}'
+}

config/file-map
@@ -1,27 +1,27 @@
-code/keybindings.json	~/.config/Code/User
-code/settings.json	~/.config/Code/User
+code/keybindings.json	$HOME/.config/Code/User
+code/settings.json	$HOME/.config/Code/User
 
-fastfetch/config.jsonc	~/.config/fastfetch
-fastfetch/dotmiku	~/.config/fastfetch	copy
+fastfetch/config.jsonc	$HOME/.config/fastfetch
+fastfetch/dotmiku	$HOME/.config/fastfetch	copy
 
-gdb/gdbinit		~/.config/gdb
-gdb/hexdump.py		~/.config/gdb
+gdb/gdbinit		$HOME/.config/gdb
+gdb/hexdump.py		$HOME/.config/gdb
 
-git/.gitconfig		~
-git/ignore		~/.config/git
+git/.gitconfig		$HOME
+git/ignore		$HOME/.config/git
 
-gpg/pgp.conf		~/.gnupg
+gpg/pgp.conf		$HOME/.gnupg
 
-mutt/.mailcap		~
-mutt/mutt_oauth2.py	~/.mutt
-mutt/.muttrc		~
-mutt/.signature		~
+mutt/.mailcap		$HOME
+mutt/mutt_oauth2.py	$HOME/.mutt
+mutt/.muttrc		$HOME
+mutt/.signature		$HOME
 
-ssh/config		~/.ssh
+ssh/config		$HOME/.ssh
 
-vim/.vimrc		~
-vim/barroit.vim		~/.vim/colors
+vim/.vimrc		$HOME
+vim/barroit.vim		$HOME/.vim/colors
 
-xmodmap/.Xmodmap	~
+xmodmap/.Xmodmap	$HOME
 
 xorg/xorg.conf		/etc/X11		copy,sudo

ubuntu/10-file-map.sh
@@ -1,0 +1,47 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log "Linking configuration files ... Skipped"
+	exit
+fi
+
+src_dir=$SCRIPT_ROOT/../assets
+
+while read line; do
+	if [[ -z "$line" ]]; then
+		continue
+	fi
+
+	name=$(r1 "$line")
+	dst_dir=$(eval "printf %s \"$(r2 "$line")\"")
+	mode=$(r3 "$line")
+
+	src=$src_dir/$name
+	dst=$dst_dir/$(basename $name)
+	cmd='ln -sf'
+
+	if printf %s $mode | grep -q copy; then
+		cmd=cp
+	fi
+
+	if printf %s $mode | grep -q sudo; then
+		cmd="sudo $cmd"
+	fi
+
+	mkdir -p $dst_dir
+
+	$cmd $src $dst
+
+	if [[ $? -eq 1 ]]; then
+		fmt="${CYAN}%-25s${RESET} -> ${RED}%s${RESET}\n"
+	else
+		fmt="${CYAN}%-25s${RESET} -> ${GREEN}%s${RESET}\n"
+	fi
+
+	printf "$fmt" $name $dst
+
+done < $CONFIG_ROOT/file-map
+
+setup_done
+log "Linking configuration files ... OK"

commit b4c8beeeb760594eb541742767744eb6c4fdfb12

libsetup: fix incorrect column split in line
Fixes: 0267f6ea77d1 ("ubuntu/file-map: automate configuration file linking")
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/libsetup.sh
@@ -22,41 +22,41 @@ exec_is_foce()
 
 v1()
 {
-	grep $2 $1 | awk '{print $2}'
+	grep $2 $1 | awk -F'\t+' '{print $2}'
 }
 
 v2()
 {
-	grep $2 $1 | awk '{print $3}'
+	grep $2 $1 | awk -F'\t+' '{print $3}'
 }
 
 v3()
 {
-	grep $2 $1 | awk '{print $4}'
+	grep $2 $1 | awk -F'\t+' '{print $4}'
 }
 
 v4()
 {
-	grep $2 $1 | awk '{print $5}'
+	grep $2 $1 | awk -F'\t+' '{print $5}'
 }
 
 
 r1()
 {
-	printf %s "$1" | awk '{print $1}'
+	printf %s "$1" | awk -F'\t+' '{print $1}'
 }
 
 r2()
 {
-	printf %s "$1" | awk '{print $2}'
+	printf %s "$1" | awk -F'\t+' '{print $2}'
 }
 
 r3()
 {
-	printf %s "$1" | awk '{print $3}'
+	printf %s "$1" | awk -F'\t+' '{print $3}'
 }
 
 r4()
 {
-	printf %s "$1" | awk '{print $4}'
+	printf %s "$1" | awk -F'\t++' '{print $4}'
 }