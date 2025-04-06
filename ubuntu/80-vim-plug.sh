92c4cc24da38a1c9f8cb97b17278f63436ffa27b
Jiamu Sun <barroit@linux.com>
2025-04-06 12:50:02 +0900

    ubuntu/vim-plug: automate vim plug installation
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ ubuntu/80-vim-plug.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log 'Installing vim plug ... Skipped'
+	exit
+fi
+
+dst=$HOME/.vim/autoload/plug.vim
+
+if [[ ! -f $dst ]]; then
+	mkdir -p $(dirname $dst)
+fi
+
+curl -Lo $dst $(cat $CONFIG_ROOT/vim-plug-url)
+
+setup_done
+log 'Installing vim plug ... OK'
