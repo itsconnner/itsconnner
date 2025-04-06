fb661aee58e015da354a0b4a49cc10e401851721
Jiamu Sun <barroit@linux.com>
2025-04-06 12:24:14 +0900

    ubuntu/gnome-terminal: automate GNOME Terminal configuration
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ ubuntu/80-terminal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done || virt; then
+	log "Configuring GNOME Terminal ... Skipped"
+	exit
+fi
+
+dconf load /org/gnome/terminal/legacy/profiles:/ < $CONFIG_ROOT/gnome-terminal
+
+setup_done
+log "Configuring GNOME Terminal ... OK"
