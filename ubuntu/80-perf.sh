ad845d91f7397fc37c4fa8d447ca29696ff079f4
Jiamu Sun <barroit@linux.com>
2025-04-07 03:01:40 +0900

    ubuntu/perf: automate perf permission configuration
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ ubuntu/80-perf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log "Configuring perf permissions ... Skipped"
+	exit
+fi
+
+printf '%s\n' 'kernel.perf_event_paranoid = -1' | \
+sudo tee /etc/sysctl.d/39-perf.conf
+
+sudo service procps reload
+
+setup_done
+log "Configuring perf permissions ... OK"
