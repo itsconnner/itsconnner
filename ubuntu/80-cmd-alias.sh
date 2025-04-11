itsconnner/itsconnner

commit f62b9228ff93fcc5d8d7e54a5d6e40b8264d79dc

ubuntu/cmd-alias: automate command aliasing
Signed-off-by: Jiamu Sun <barroit@linux.com>

ubuntu/80-cmd-alias.sh
@@ -1,0 +1,28 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log "Aliasing commands ... Skipped"
+	exit
+fi
+
+touch ~/.bash_aliases
+
+while read line; do
+	if [[ ! $line || $line == '#'* ]]; then
+		continue
+	fi
+
+	name=$(r1 "$line")
+	cmd=$(r2 "$line")
+	prefix="alias $name"
+
+	if grep -q $name ~/.bash_aliases; then
+		sed -i "s|^$prefix='.*'$|$prefix='$cmd'|" ~/.bash_aliases
+	else
+		printf '\n%s\n' "$prefix='$cmd'" >> ~/.bash_aliases
+	fi
+done < $CONFIG_ROOT/cmd-alias
+
+setup_done
+log "Aliasing commands ... OK"

config/cmd-alias
@@ -1,0 +1,23 @@
+add		git add
+am		git am
+amend		git commit --amend
+apply		git apply
+branch		git branch
+commit		git commit
+format-patch	git format-path
+log		git log
+merge		git merge
+merge-base	git merge-base
+pull		git pull
+push		git push
+rebase		git rebase
+reflog		git reflog
+show		git show
+stash		git stash
+status		git status
+switch		git switch
+
+m		make
+g		gdb
+
+use-idf		. $HOME/git/esp-idf/export.sh

commit e3fc8dbbb21e135b36ec6f8c231cf9508a79f442

ubuntu/cmd-alias: fix incorrect prefix matching in alias map
Fixes: f62b9228ff93 ("ubuntu/cmd-alias: automate command aliasing")
Signed-off-by: Jiamu Sun <barroit@linux.com>

ubuntu/80-cmd-alias.sh
@@ -17,7 +17,7 @@ while read line; do
 	cmd=$(r2 "$line")
 	prefix="alias $name"
 
-	if grep -q $name ~/.bash_aliases; then
+	if grep -q "$prefix=" ~/.bash_aliases; then
 		sed -i "s|^$prefix='.*'$|$prefix='$cmd'|" ~/.bash_aliases
 	else
 		printf '\n%s\n' "$prefix='$cmd'" >> ~/.bash_aliases