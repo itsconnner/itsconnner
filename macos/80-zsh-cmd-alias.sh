itsconnner/itsconnner

commit a1830e64b9e4ea8dfe37947bcc56eb144dc6e9ea

macos/zsh-cmd-alias: automate command aliasing
Signed-off-by: Jiamu Sun <barroit@linux.com>

macos/80-zsh-cmd-alias.sh
@@ -1,0 +1,39 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log 'Aliasing commands ... Skipped'
+	exit
+fi
+
+touch ~/.zshrc
+touch ~/.zsh_aliases
+
+while read line; do
+	if [[ ! $line || $line == '#'* ]]; then
+		continue
+	fi
+
+	name=$(r1 "$line")
+	cmd=$(r2 "$line")
+	prefix="alias $name"
+
+	if grep -q "$prefix=" ~/.zsh_aliases; then
+		sed -i '' "s|^$prefix='.*'$|$prefix='$cmd'|" ~/.zsh_aliases
+	else
+		printf '\n%s\n' "$prefix='$cmd'" >> ~/.zsh_aliases
+	fi
+done < $CONFIG_ROOT/cmd-alias
+
+read -d '' lines <<EOF
+if [ -f ~/.zsh_aliases ]; then
+	. ~/.zsh_aliases
+fi
+EOF
+
+if ! grep -Fxq "$lines" ~/.zshrc; then
+	printf '\n%s\n' "$lines" >> ~/.zshrc
+fi
+
+setup_done
+log 'Aliasing commands ... OK'

commit 7288fc524d2a7e77b5eebb19f8ab7165c7cfd2c1

macos/zsh-cmd-alias: fix incorrect shebang shell
Fixes: a1830e64b9e4 ("macos/zsh-cmd-alias: automate command aliasing")
Signed-off-by: Jiamu Sun <barroit@linux.com>

macos/80-zsh-cmd-alias.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 if ! exec_is_foce && setup_is_done; then