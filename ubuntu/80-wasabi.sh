itsconnner/itsconnner

commit 7b53676a214930cb3762fb70c2bc6e0cf7b809b5

ubuntu/wasabi: automate wasabi configuration
Signed-off-by: Jiamu Sun <barroit@linux.com>

ubuntu/80-wasabi.sh
@@ -1,0 +1,44 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! exec_is_foce && setup_is_done; then
+	log 'Configuring Wasabi cloud storage ... Skipped'
+	exit
+fi
+
+sysd=$HOME/.config/systemd/user
+secret=/media/$(whoami)/secret
+
+if [[ ! -d $secret ]]; then
+	die "GPG file storage didn't mount to \`$secret'"
+fi
+
+gpg --yes -o $HOME/.config/rclone/rclone.conf -d $secret/rclone.conf.gpg
+
+if [[ $? -ne 0 ]]; then
+	die "Importing rclone.conf ... Failed"
+fi
+
+mkdir -p $sysd
+
+cat <<EOF > $sysd/wasabi.service
+[Unit]
+After=default.target
+
+[Service]
+ExecStart=%h/git/barroit/scripts/mount-wasabi.sh
+ExecStop=%h/git/barroit/scripts/unmount-wasabi.sh
+RemainAfterExit=yes
+Type=oneshot
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl --user daemon-reload
+systemctl --user enable $sysd/wasabi.service
+
+$CONFIG_ROOT/../scripts/mount-wasabi.sh
+
+setup_done
+log 'Configuring Wasabi cloud storage ... OK'

scripts/mount-wasabi.sh
@@ -1,0 +1,10 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if ! mount | grep -q wasabi:barroit; then
+	rclone mount --daemon wasabi:barroit $HOME/wasabi
+fi
+
+if ! mount | grep -q wasabi:cred; then
+	rclone mount --daemon --vfs-cache-mode=full wasabi:cred $HOME/cred
+fi

scripts/umount-wasabi.sh
@@ -1,0 +1,10 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if mount | grep -q wasabi:barroit; then
+	umount $HOME/wasabi
+fi
+
+if mount | grep -q wasabi:cred; then
+	umount $HOME/cred
+fi

commit f1c592b51200635f14cb6ab6cdb3e7ba30beca8c

ubuntu/wasabi: fix missing gpg output directory
Fixes: 7b53676a2149 ("ubuntu/wasabi: automate wasabi configuration")
Signed-off-by: Jiamu Sun <barroit@linux.com>

ubuntu/80-wasabi.sh
@@ -13,6 +13,7 @@ if [[ ! -d $secret ]]; then
 	die "GPG file storage didn't mount to \`$secret'"
 fi
 
+mkdir -p $HOME/.config/rclone
 gpg --yes -o $HOME/.config/rclone/rclone.conf -d $secret/rclone.conf.gpg
 
 if [[ $? -ne 0 ]]; then