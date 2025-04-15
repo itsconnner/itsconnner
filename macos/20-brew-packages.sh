5173e795f8e165f0f55229b6d2248309e7e0ed1d
Jiamu Sun <barroit@linux.com>
2025-04-15 16:41:20 +0900

    macos/brew-packages: support adding new PATH entries in config file
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- macos/20-brew-packages.sh
+++ macos/20-brew-packages.sh
@@ -7,6 +7,7 @@ if ! exec_is_foce && setup_is_done; then
 fi
 
 brew update
+touch .zshrc
 
 while read line; do
 	if [[ ! $line || $line == '#'* ]]; then
@@ -15,11 +16,20 @@ while read line; do
 
 	name=$(r1 "$line")
 	cask=$(r2 "$line")
+	prefix=$(r3 "$line")
 
 	if [[ $cask && $cask == 'cask' ]]; then
 		brew install --cask $name
 	else
 		brew install $name
+	fi
+
+	if [[ $prefix ]]; then
+		exp="export PATH=\"$prefix:\$PATH\""
+
+		if ! grep -q "$exp" ~/.zshrc; then
+			printf '\n%s\n' "$exp" >> ~/.zshrc
+		fi
 	fi
 done < $CONFIG_ROOT/brew-packages
 
