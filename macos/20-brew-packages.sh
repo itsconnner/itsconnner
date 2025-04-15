itsconnner/itsconnner

commit 5173e795f8e165f0f55229b6d2248309e7e0ed1d

macos/brew-packages: support adding new PATH entries in config file
Signed-off-by: Jiamu Sun <barroit@linux.com>

macos/20-brew-packages.sh
@@ -7,6 +7,7 @@ if ! exec_is_foce && setup_is_done; then
 fi
 
 brew update
+touch .zshrc
 
 while read line; do
 	if [[ ! $line || $line == '#'* ]]; then
@@ -15,11 +16,20 @@ while read line; do
 
 	name=$(r1 "$line")
 	cask=$(r2 "$line")
+	prefix=$(r3 "$line")
 
 	if [[ $cask && $cask == 'cask' ]]; then
 		brew install --cask $name
 	else
 		brew install $name
+	fi
+
+	if [[ $prefix ]]; then
+		exp="export PATH=\"$prefix:\$PATH\""
+
+		if ! grep -q "$exp" ~/.zshrc; then
+			printf '\n%s\n' "$exp" >> ~/.zshrc
+		fi
 	fi
 done < $CONFIG_ROOT/brew-packages
 

config/brew-packages
@@ -2,6 +2,7 @@ discord			cask
 fastfetch
 gnupg
 google-chrome		cask
+make			-	/opt/homebrew/opt/make/libexec/gnubin
 mutt
 visual-studio-code	cask
 vmware-fusion		cask

commit 7052a2805d47f8a366c44a097235d73a7e846098

macos/brew-packages: fix incorrect path to ~/.zshrc
Fixes: 5173e795f8e1 ("macos/brew-packages: support adding new PATH entries in config file")
Signed-off-by: Jiamu Sun <barroit@linux.com>

macos/20-brew-packages.sh
@@ -7,7 +7,7 @@ if ! exec_is_foce && setup_is_done; then
 fi
 
 brew update
-touch .zshrc
+touch ~/.zshrc
 
 while read line; do
 	if [[ ! $line || $line == '#'* ]]; then